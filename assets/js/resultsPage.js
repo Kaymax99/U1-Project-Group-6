8c0b47c74983258546bc52e98b5ca5e564da0d3d
Simone Basile <simonebasile199@gmail.com>
2022-12-20 13:13:27 +0100

    Fixed base donut chart (ty Marco)

--- assets/js/resultsPage.js
+++ assets/js/resultsPage.js
@@ -1,11 +1,11 @@
-let data = anychart.data.set([
+/* let data = anychart.data.set([
   ["Wrong Answers", 33.3],
   ["Right Answers", 66.7],
 ]);
 
 let chart = anychart.pie(data);
 chart.innerRadius("75%");
-var palette = anychart.palettes.distinctColors();
+let palette = anychart.palettes.distinctColors();
 palette.items([{ color: "#c2128d" }, { color: "#00ffff" }]);
 chart.palette(palette);
 chart.background("none");
@@ -15,7 +15,7 @@ chart.labels().format("{%x} — {%y}%").fontSize(16);
 chart.legend(false);
 chart.tooltip().format("Percentage: {%PercentValue}%");
 
-var label = anychart.standalones.label();
+let label = anychart.standalones.label();
 label
   .useHtml(true)
   .text(
@@ -34,4 +34,56 @@ label
 chart.center().content(label);
 
 chart.container("chartContainer");
-chart.draw();
+chart.draw(); */
+
+// setup
+const data = {
+  labels: ["Wrong", "Right"],
+  datasets: [
+    {
+      label: "Test Results",
+      data: [33.3, 66.7],
+      backgroundColor: ["#c2128d", "#00ffff"],
+      borderWidth: 0,
+      hoverOffset: 4,
+      cutout: "72%",
+    },
+  ],
+};
+
+// centerText plugin
+const centerText = {
+  id: "centerText",
+  afterDatasetsDraw(chart, args, options) {
+    const {
+      ctx,
+      chartArea: { top, right, bottom, left, width, height },
+    } = chart;
+    ctx.save();
+    console.log(top);
+    ctx.font = "bolder 30px Arial";
+    ctx.fillStyle = "#c2128d";
+    ctx.textAlign = "center";
+    ctx.fillText("Sales:", 200, 200);
+  },
+};
+
+// config
+const config = {
+  type: "doughnut",
+  data: data,
+  options: {
+    plugins: {
+      text: [centerText],
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  },
+};
+
+// render initial block
+const myChart = new Chart(document.getElementById("myChart"), config);
