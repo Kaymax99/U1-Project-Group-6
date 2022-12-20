9be07b7db62ce153967a7b1ad89a1083ac423044
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-20 13:57:04 +0100

    href

--- assets/js/welcomePage.js
+++ assets/js/welcomePage.js
@@ -2,7 +2,11 @@ const goOnIfChecked = () => {
   const checkBox = document.getElementById("customCheck");
   const botton = document.getElementById("proceed");
   if (checkBox.checked) {
+<<<<<<< Updated upstream
     location.href = "testpage.html";
+=======
+    location.href = "testPage.html";
+>>>>>>> Stashed changes
   } else {
     location.href = "#";
   }
