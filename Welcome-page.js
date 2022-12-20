e07e57b0e9e775626d036090c702163818df33d0
Simone Basile <simonebasile199@gmail.com>
2022-12-20 10:08:12 +0100

    Fixed reset and adjusted directory order

--- Welcome-page.js
+++
@@ -1,9 +0,0 @@
-const goOnIfChecked = () => {
-  const checkBox = document.getElementById("customCheck");
-  const botton = document.getElementById("proceed");
-  if (checkBox.checked) {
-    location.href = "https://www.geeksforgeeks.org/";
-  } else {
-    location.href = "#";
-  }
-};
