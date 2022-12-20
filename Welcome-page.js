40ff9a4fd0dc68d9cddf0de64a5d795376209d08
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-20 09:31:54 +0100

    Added link to testPage

--- Welcome-page.js
+++ Welcome-page.js
@@ -2,7 +2,7 @@ const goOnIfChecked = () => {
   const checkBox = document.getElementById("customCheck");
   const botton = document.getElementById("proceed");
   if (checkBox.checked) {
-    location.href = "https://www.geeksforgeeks.org/";
+    location.href = "testpage.html";
   } else {
     location.href = "#";
   }
