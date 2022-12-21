46d41843f166752072f53ce087a4ff6365a0cd03
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-21 14:29:13 +0100

    color text edited

--- assets/js/welcomePage.js
+++ assets/js/welcomePage.js
@@ -2,11 +2,7 @@ const goOnIfChecked = () => {
   const checkBox = document.getElementById("customCheck");
   const botton = document.getElementById("proceed");
   if (checkBox.checked) {
-<<<<<<< Updated upstream
-    location.href = "testpage.html";
-=======
     location.href = "testPage.html";
->>>>>>> Stashed changes
   } else {
     location.href = "#";
   }
